21ad32cfab7bb0d20e9e21eebfaba0bf390ada24
Kacper Stojek <kacper.stojek22@gmail.com>
2023-08-13 22:16:04 +0200

    src/include/gpio.h: Add the mising uint8 cast
    Signed-off-by: Kacper Stojek <kacper.stojek22@gmail.com>

--- src/include/gpio.h
+++ src/include/gpio.h
@@ -1,10 +1,12 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#include <stdint.h>
+
 void setup_gpio();
 
 #define CAST_PINS_TO_UINT8(p0, p1, p2, p3, p4, p5, p6, p7)  \
-    (p0 | \
+    (uint8_t)(p0 | \
     (p1 << 1) | \
     (p2 << 2) | \
     (p3 << 3) | \
